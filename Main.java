86ad77fc94f4ccc6d87bd2ac06a23090c435bd9f
JanePreethi <janepreethi37@gmail.com>
2026-05-05 13:59:52 +0530

    Add Main class with basic exception handling

---
+++ Main.java
@@ -0,0 +1,11 @@
+package crud;
+public class Main {
+	public static void main(String[] args) {
+		try {
+			Dashboard db = new Dashboard();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
